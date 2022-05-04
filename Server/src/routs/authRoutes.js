a21679c1e69cf3a6e6170c64f9090eb8d975b660
aed1oN <eicanovic1@etf.unsa.ba>
2022-05-04 22:01:57 +0200

    spremanje logova za prijavu, registraciju, dodavanje i registraciju korisnika

--- Server/src/routs/authRoutes.js
+++ Server/src/routs/authRoutes.js
@@ -1,73 +1,142 @@
-const express = require('express'); 
-const mongoose = require('mongoose');
-const jwt = require('jsonwebtoken')
+const express = require("express");
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
 
-const User = mongoose.model('User');
+const User = mongoose.model("User");
+const Log = require("../models/Log.js");
 
 const router = express.Router();
 
- router.post('/signup', async (req, res)=>{
+router.post("/singout", async (req, res) => {
+  let user = req.body;
+  let tipKorisnika = "Korisnik";
+  if (user.email === "admin") tipKorisnika = "Admin";
 
-    const {email, password, ime, prezime, jmbg, omiljenaBoja, omiljenaZivotinja} = req.body;
+  const log = new Log({
+    korisnikId: user.id,
+    korisnikEmail: user.email,
+    tipKorisnika: tipKorisnika,
+    vrijeme: new Date().toLocaleString("en-GB"),
+    opisAkcije: `Odjava korisnika '${user.email}'`,
+  });
 
-    try{
+  try {
+    await log.save();
+    res.status(200).send({});
+  } catch (err) {
+    console.error(err);
+    res.status(422).send({ err });
+  }
+});
 
-    const user = new User({email, password, ime, prezime, jmbg, omiljenaBoja, omiljenaZivotinja});
+router.post("/signup", async (req, res) => {
+  const {
+    email,
+    password,
+    ime,
+    prezime,
+    jmbg,
+    omiljenaBoja,
+    omiljenaZivotinja,
+  } = req.body;
+
+  try {
+    const user = new User({
+      email,
+      password,
+      ime,
+      prezime,
+      jmbg,
+      omiljenaBoja,
+      omiljenaZivotinja,
+    });
+    const log = new Log({
+      korisnikId: user._id,
+      korisnikEmail: user.email,
+      tipKorisnika: "Korisnik",
+      vrijeme: new Date().toLocaleString("en-GB"),
+      opisAkcije: `Korisnik '${user.email}' uspjesno registrovan`,
+    });
     await user.save();
+    await log.save();
 
-    const token = jwt.sign({userId: user._id}, 'MY_SECRET_KEY');
-    res.send({token});
+    const token = jwt.sign({ userId: user._id }, "MY_SECRET_KEY");
+    res.send({ token, id: user._id, email: user.email });
+  } catch (err) {
+    res.status(422).send(err.message);
+  }
+});
 
-    }catch(err){
-        res.status(422).send(err.message);
-    }
+router.post("/dodaj", async (req, res) => {
+  const {
+    email,
+    password,
+    ime,
+    prezime,
+    jmbg,
+    omiljenaBoja,
+    omiljenaZivotinja,
+  } = req.body;
 
-
- });
-
- router.post('/dodaj', async (req, res)=>{
-
-    const {email, password, ime, prezime, jmbg, omiljenaBoja, omiljenaZivotinja} = req.body;
-
-    try{
-
-    const user = new User({email, password, ime, prezime, jmbg, omiljenaBoja, omiljenaZivotinja});
+  try {
+    const user = new User({
+      email,
+      password,
+      ime,
+      prezime,
+      jmbg,
+      omiljenaBoja,
+      omiljenaZivotinja,
+    });
+    const log = new Log({
+      korisnikId: user._id,
+      korisnikEmail: user.email,
+      tipKorisnika: "Admin",
+      vrijeme: new Date().toLocaleString("en-GB"),
+      opisAkcije: `Korisnik '${user.email}' uspjesno dodan od strane administratora`,
+    });
     await user.save();
+    await log.save();
 
-    const token = jwt.sign({userId: user._id}, 'MY_SECRET_KEY');
-    res.send({token});
+    const token = jwt.sign({ userId: user._id }, "MY_SECRET_KEY");
+    res.send({ token, id: user._id, email: user.email });
+  } catch (err) {
+    res.status(422).send({ error: "greska" });
+  }
+});
 
-    }catch(err){
-        res.status(422).send({error:"greska"});
-    }
+router.post("/signin", async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res
+      .status(422)
+      .send({ error: "Polja email i password ne smiju biti prazna" });
+  }
 
+  const user = await User.findOne({ email });
 
- });
+  if (!user) {
+    return res.status(422).send({ error: "Nepravilan email ili password" });
+  }
 
- router.post('/signin', async (req, res)=>{
+  try {
+    let korisnik = "Korisnik";
+    if (user.email === "admin") korisnik = "Admin";
 
-    const {email, password} = req.body;
+    const log = new Log({
+      korisnikId: user._id,
+      korisnikEmail: user.email,
+      tipKorisnika: korisnik,
+      vrijeme: new Date().toLocaleString("en-GB"),
+      opisAkcije: `Prijava korisnika '${user.email}'`,
+    });
+    await user.comparePassword(password);
+    const token = jwt.sign({ userId: user._id }, "MY_SECRET_KEY");
+    await log.save();
+    res.send({ token, id: user._id, email: user.email });
+  } catch (err) {
+    return res.status(422).send({ error: "Nepravilan email ili password" });
+  }
+});
 
-    if(!email || !password){
-        return res.status(422).send({error:'Polja email i password ne smiju biti prazna'});
-    }
-
-    const user = await User.findOne({email});
-
-    if(!user){
-        return res.status(422).send({error:'Nepravilan email ili password'});
-    }
-
-    try{
-        await user.comparePassword(password)
-        const token = jwt.sign({userId: user._id}, 'MY_SECRET_KEY');
-        res.send({token});
-    }catch(err){
-        return res.status(422).send({error:'Nepravilan email ili password'});
-    }
-
-
-
- });
-
- module.exports=router;
+module.exports = router;
--- Server/src/server.js
+++ Server/src/server.js
@@ -1,9 +1,9 @@
-require('./models/Users');
-const express = require('express');
-const mongoose = require('mongoose');
-const authRouts = require('./routs/authRoutes');
-const bodyParser = require('body-parser');
-const requireAuth = require('./middlewares/requireAuth');
+require("./models/Users");
+const express = require("express");
+const mongoose = require("mongoose");
+const authRouts = require("./routs/authRoutes");
+const bodyParser = require("body-parser");
+const requireAuth = require("./middlewares/requireAuth");
 
 const app = express();
 
@@ -11,31 +11,18 @@ app.use(bodyParser.json());
 
 app.use(authRouts);
 
-const mongoUri = 'mongodb+srv://admin:admin@cluster0.a9vmk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+const mongoUri =
+  "mongodb+srv://admin:admin@cluster0.a9vmk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
-mongoose.connect(mongoUri,{
-
-
+mongoose.connect(mongoUri, (err) => {
+  if (err) console.error(err);
+  else console.log("Connected to DB");
 });
 
-mongoose.connection.on('connected', ()=>{
-
-console.log("connected to mongo instnce");
-
+app.get("/", requireAuth, (req, res) => {
+  res.send(`Your email: ${req.user.email}`);
 });
 
-mongoose.connection.on('error', (err)=>{
-
-    console.error("connected to mongo instnce", err);
-    
-});
-
-app.get("/", requireAuth, (req, res) =>{
-
-    res.send(`Your email: ${req.user.email}`);
-
-});
-
-app.listen(3000, function(){
-    console.log("Server radina portu: "+ 3000);
+app.listen(3000, function () {
+  console.log("Server radina portu: " + 3000);
 });
