273f5d075ca93aac551cb931f05207df7996df23
Edin <edin.icanovic@gmail.com>
2022-05-10 22:31:58 +0200

    promjene u dizajnu

--- app/api/tracker.js
+++ app/api/tracker.js
@@ -1,7 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-
-  baseURL: "http://5fc9-80-80-40-35.ngrok.io",
-
+  baseURL: "https://5f9b-2a02-27b0-4400-e160-916f-f187-f001-ea42.eu.ngrok.io",
 });
--- app/screens/ListaProizvodaScreen.js
+++ app/screens/ListaProizvodaScreen.js
@@ -1,108 +1,130 @@
-import React, { useContext} from 'react';
-import { useFocusEffect } from '@react-navigation/native';
-import {StyleSheet, Text, View, SafeAreaView, FlatList, Alert} from 'react-native';
-import { Context as AuthContext } from '../context/AuthContext'
-import { TouchableOpacity } from 'react-native-web';
+import React, { useContext } from "react";
+import { useFocusEffect } from "@react-navigation/native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  FlatList,
+  Alert,
+} from "react-native";
+import { Context as AuthContext } from "../context/AuthContext";
+import { ListItem, Icon } from "@rneui/base";
+import { TouchableOpacity } from "react-native-web";
 const { useState } = React;
 
 const ListaProizvodaScreen = ({ navigation }) => {
+  const [lista, setLista] = useState([]);
+  const { state, korisnikPod, proizvodPod } = useContext(AuthContext);
 
-    const [lista, setLista] = useState([]);
-    const {state, korisnikPod, proizvodPod} = useContext(AuthContext);
+  useFocusEffect(
+    React.useCallback(() => {
+      setLista(state.list);
+    }, [])
+  );
 
-    useFocusEffect(
-        React.useCallback(() => {
+  const getProizvod = (name) => {
+    proizvodPod(name);
+  };
 
-        setLista(state.list);
+  const ItemRender = ({ proizvod }) => (
+    <ListItem>
+      <ListItem.Content>
+        <ListItem.Title>
+          <Text>{proizvod.naziv}</Text>
+        </ListItem.Title>
+        <ListItem.Subtitle>
+          <Text>
+            Kolicina: {proizvod.kolicina}
+            {"\n"}
+            Jedinica: {proizvod.jedinica}
+          </Text>
+        </ListItem.Subtitle>
+      </ListItem.Content>
+      <Icon
+        raised
+        name="edit"
+        size={14}
+        type="font-awesome"
+        onPress={() => getProizvod(proizvod.naziv)}
+      />
+    </ListItem>
 
-        }, [])
-      );
-      
-      const getProizvod = (name) => {
-        proizvodPod(name)
-      }
-
-
-      const ItemRender = ({ proizvod }) => (
-        <SafeAreaView style={styles.item}>
-
-        <SafeAreaView style={{flex: 1}}>
-            <Text style={styles.text} onPress={()=>getProizvod(proizvod.naziv)}> {proizvod.naziv}</Text>
-        </SafeAreaView>
-
-        <SafeAreaView style={{flex: 1}}>
-            <Text style={styles.text2} onPress={()=>getProizvod(proizvod.naziv)}>  {proizvod.kolicina}</Text>
-        </SafeAreaView>
-
-        <SafeAreaView style={{flex: 1, paddingRight:15}}>
-            <Text style={styles.text3} onPress={()=>getProizvod(proizvod.naziv)}>{proizvod.jedinica}  </Text>
-        </SafeAreaView>
-
-        </SafeAreaView>
-      );
-  
-    return (
-      <SafeAreaView style={styles.container}>
-        
-        <Text style={styles.item2}> NAZIV        KOLICINA     JEDINICA</Text>
-
-        <FlatList
-          style={styles.listaa}
-          data={lista}
-          renderItem={({ item }) => <ItemRender proizvod={item.proizvod} />}
-          keyExtractor={item => item.proizvod.id}
-        />
-
+    /*
+    <SafeAreaView style={styles.item}>
+      <SafeAreaView style={{ flex: 1 }}>
+        <Text style={styles.text} onPress={() => getProizvod(proizvod.naziv)}>
+          {" "}
+          {proizvod.naziv}
+        </Text>
       </SafeAreaView>
-    );
-  }
+
+      <SafeAreaView style={{ flex: 1 }}>
+        <Text style={styles.text2} onPress={() => getProizvod(proizvod.naziv)}>
+          {" "}
+          {proizvod.kolicina}
+        </Text>
+      </SafeAreaView>
+
+      <SafeAreaView style={{ flex: 1, paddingRight: 15 }}>
+        <Text style={styles.text3} onPress={() => getProizvod(proizvod.naziv)}>
+          {proizvod.jedinica}{" "}
+        </Text>
+      </SafeAreaView>
+    </SafeAreaView>*/
+  );
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.item2}>Proizvodi u skladištu</Text>
+
+      <FlatList
+        style={styles.listaa}
+        data={lista}
+        renderItem={({ item }) => <ItemRender proizvod={item.proizvod} />}
+        keyExtractor={(item) => item.proizvod.id}
+      />
+    </SafeAreaView>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'white',
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginTop: "10%"
+    backgroundColor: "white",
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: "10%",
   },
   text: {
     flex: 1,
     color: "#4a4b44",
     fontSize: 22,
-    textAlign: 'left'  
-},
+    textAlign: "left",
+  },
   text2: {
     flex: 1,
     color: "#4a4b44",
     fontSize: 22,
-    textAlign: 'center',
+    textAlign: "center",
   },
   text3: {
     flex: 1,
     color: "#4a4b44",
     fontSize: 22,
-    textAlign: 'right'
+    textAlign: "right",
   },
-  listaa:{
-    width:"90%",
-  },
-  item: {
-    backgroundColor: '#89cff0',
-    fontSize: 20,
-    padding: 10,
-    flex: 1, flexDirection: 'row', alignItems: 'center',
-    marginVertical: 5,
+  listaa: {
+    width: "90%",
   },
   item2: {
-    backgroundColor: '#89cff0',
+    textAlign: "center",
     fontSize: 20,
     padding: 10,
-     alignItems: 'center',
+    alignItems: "center",
     marginVertical: 8,
-    width:"90%"
+    width: "90%",
   },
 });
-
-
 
 export default ListaProizvodaScreen;
